020bf98706a8d2e20df374010cf5f2ecce8252f3
nanasi-1 <test_email>
2023-10-24 12:17:59 +0900

    回答完了後のダイアログを追加

--- content-script.js
+++ content-script.js
@@ -33,7 +33,7 @@ window.addEventListener('urlChange', async function() {
             // 回答時に正解か出力するやつ
             que.querySelectorAll('.answers>li').forEach(li => {
                 // onclickなのはremoveEventListenerのせい
-                li.onclick = () => {
+                li.onclick = async () => {
                     const isFolded = li.closest('li.exercise-item').classList.contains('is-folded');
                     if (!isFolded) {
                         console.log(new Date()); // 区切りとして日付を出力
@@ -42,6 +42,21 @@ window.addEventListener('urlChange', async function() {
                             if (!answer.classList.contains('answers-selected')) console.log(answer);
                         }
                         console.log(JSON.parse(li.dataset.correct) ? '正解' : '不正解');
+
+                        await sleep(100);
+                        const isAll = Array.from(que.querySelectorAll('.answers').values()).every(ul => {
+                            return Array.from(ul.childNodes.values()).find(li => li.classList.contains('answers-selected'));
+                        });
+                        if (isAll) {
+                            console.log('回答完了');
+                            const arr = [];
+                            for (let answer of answers) {
+                                if (!answer.classList.contains('answers-selected')) {
+                                    arr.push(answer.parentElement.previousSibling.innerText);
+                                }
+                            }
+                            alert(arr.length ? `間違っている問いがあります:\n${arr.join('\n')}` : '間違っている問いはありません');
+                        }
                     }
                 };
             });
